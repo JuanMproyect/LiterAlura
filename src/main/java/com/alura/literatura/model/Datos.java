e09fc09da85e89f365e3b060c64bdac39ef41f77
Juan Morel <juanzmorel155@gmail.com>
2024-07-10 19:17:21 -0600

    Datos-Model

--- src/main/java/com/alura/literatura/model/Datos.java
+++ src/main/java/com/alura/literatura/model/Datos.java
@@ -1,2 +1,11 @@
-package com.alura.literatura.model;public class Datos {
+package com.alura.literatura.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Datos(
+        @JsonAlias("results") List<DatosLibros> resultados) {
 }
