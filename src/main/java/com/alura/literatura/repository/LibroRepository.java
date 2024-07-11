7985d498224d4fd9d1d03c877790516477bc0b90
Juan Morel <juanzmorel155@gmail.com>
2024-07-10 19:24:56 -0600

    Repository

--- src/main/java/com/alura/literatura/repository/LibroRepository.java
+++ src/main/java/com/alura/literatura/repository/LibroRepository.java
@@ -1,2 +1,15 @@
-package com.alura.literatura.repository;public interface LibroRepository {
+package com.alura.literatura.repository;
+import com.alura.literatura.model.Idioma;
+import com.alura.literatura.model.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface LibroRepository extends JpaRepository<Libro, Long> {
+    Optional<Libro> findByTituloIgnoreCase(String titulo);
+
+    List<Libro> findByIdioma(Idioma idioma);
 }
