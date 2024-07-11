fcc962223355eb1b6da1411094bee84a2308678f
Juan Morel <juanzmorel155@gmail.com>
2024-07-10 19:35:39 -0600

    Update service

--- src/main/java/com/alura/literatura/service/IConvierteDatos.java
+++ src/main/java/com/alura/literatura/service/IConvierteDatos.java
@@ -1,5 +1,5 @@
 package com.alura.literatura.service;
 
 public interface IConvierteDatos {
-    <T> T obtenerDatos(String json, Class<T> clase); 
+    <T> T obtenerDatos(String json, Class<T> clase);  
 }
