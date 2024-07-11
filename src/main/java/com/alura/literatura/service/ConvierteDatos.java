96b9d9fe8409253c5426c9b26e5db9ecc60f7a89
Juan Morel <juanzmorel155@gmail.com>
2024-07-10 19:28:02 -0600

    ConvierteDatos-Service

--- src/main/java/com/alura/literatura/service/ConvierteDatos.java
+++ src/main/java/com/alura/literatura/service/ConvierteDatos.java
@@ -1,2 +1,15 @@
-package com.alura.literatura.service;public class ConvierteDatos {
+package com.alura.literatura.service;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConvierteDatos {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public <T> T obtenerDatos(String json, Class<T> clase) {
+        try {
+            return objectMapper.readValue(json, clase);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
