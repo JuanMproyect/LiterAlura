dfe9cdb06f588d96b20227c94dc99ccffe31c2b1
Juan Morel <juanzmorel155@gmail.com>
2024-07-10 19:12:28 -0600

    Ejecutable

--- src/main/java/com/alura/literatura/LiteraturaApplication.java
+++ src/main/java/com/alura/literatura/LiteraturaApplication.java
@@ -1,13 +1,28 @@
 package com.alura.literatura;
 
+import com.alura.literatura.principal.Principal;
+import com.alura.literatura.repository.AutorRepository;
+import com.alura.literatura.repository.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraturaApplication {
+public class LiteraturaApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LiteraturaApplication.class, args);
-	}
+    @Autowired
+    private AutorRepository autorRepository;
+    @Autowired
+    private LibroRepository libroRepository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(LiteraturaApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Principal principal = new Principal(autorRepository, libroRepository);
+        principal.muestraMenu();
+    }
 }
