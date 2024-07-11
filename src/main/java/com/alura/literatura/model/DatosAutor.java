ecfcc039ee6ae2cd33530e4a8231ce85894cf9de
Juan Morel <juanzmorel155@gmail.com>
2024-07-10 19:17:51 -0600

    DatosAutor-Model

--- src/main/java/com/alura/literatura/model/DatosAutor.java
+++ src/main/java/com/alura/literatura/model/DatosAutor.java
@@ -1,2 +1,11 @@
-package com.alura.literatura.model;public class DatosAutor {
+package com.alura.literatura.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosAutor(
+        @JsonAlias("name") String nombre,
+        @JsonAlias("birth_year") int fechaNacimiento,
+        @JsonAlias("death_year") int fechaMuerte) {
 }
