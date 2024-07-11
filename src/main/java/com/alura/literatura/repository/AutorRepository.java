5fefc19228e62009dea5378174396592de966930
Juan Morel <juanzmorel155@gmail.com>
2024-07-10 19:24:34 -0600

    Repository

--- src/main/java/com/alura/literatura/repository/AutorRepository.java
+++ src/main/java/com/alura/literatura/repository/AutorRepository.java
@@ -1,2 +1,17 @@
-package com.alura.literatura.repository;public interface AutorRepository {
+package com.alura.literatura.repository;
+import com.alura.literatura.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+    Optional<Autor> findByNombreIgnoreCase(String nombre);
+
+    @Query("SELECT a FROM Autor a WHERE a.fechaNacimiento <= :año AND a.fechaMuerte >= :año")
+    List<Autor> autoresVivosEnUnDeterminadoAño(@Param("año") Integer año);
 }
